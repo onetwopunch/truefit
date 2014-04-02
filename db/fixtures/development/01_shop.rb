6a33132cc9b8f22ff1eb835cb010dad254f38c09
Ryan <jrcanty@gmail.com>
2014-04-01 22:39:09 -0700

    added yelp to db seed

--- db/fixtures/development/01_shop.rb
+++ db/fixtures/development/01_shop.rb
@@ -4,8 +4,7 @@ Shop.seed do |shop|
     shop.phone = '(619) 450 6031'
     shop.email = "#{name}@gmail.com"
     shop.instagram = shop.facebook = shop.twitter = name
+    shop.yelp = "true-fit-tattoo-san-diego-4"
     shop.address = '6561 El Cajon Blvd., San Diego, CA, 92115'
     shop.hours = 'Monday-Saturday: 12-8pm, Sunday: Appointment Only'
-    
-    
 end
