d9f791b565a7c10d28d3227e7240229bef487704
ecesurfer <jrcanty@gmail.com>
2014-04-20 01:33:42 +0000

    fixed root route

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ TrueFitTattoo::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'home#index'
+  root :to => 'home#index'
   post ':controller(/:action(/:id(.:format)))'
   get ':controller(/:action(/:id(.:format)))'
 end
