4fc8342c65375737d4b2049136b57ef89c794fd3
ecesurfer <jrcanty@gmail.com>
2014-04-20 01:25:06 +0000

    fixed session store?

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
-TrueFitTattoo::Application.config.session_store :cookie_store, key: '_TrueFitTattoo_session'
+TrueFitTattoo::Application.config.session_store :cookie_store, :key => '_TrueFitTattoo_session'
